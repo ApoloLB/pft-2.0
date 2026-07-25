bf1395883650fb0c5dddeb156c1486c5668b10a8
apololb <apololbastos@gmail.com>
2026-07-25 14:02:23 -0300

    feat(home): add basic structure for home section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HomeComponent } from './modules/portifolio/pages/home/home.component';
+import { AboutComponent } from './modules/portifolio/pages/about/about.component';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
